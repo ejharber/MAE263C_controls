7ba070e14ef73b53fd7dbaadf11417386bbcb4ac
ejharber <ejharber@gmail.com>
2022-05-09 10:31:03 -0700

    velocity transformation

--- edge-following/scripts/velocity_controller_test.py
+++ edge-following/scripts/velocity_controller_test.py
@@ -6,6 +6,8 @@ import rospy
 import tf2_msgs.msg
 import kinova_msgs.msg
 import tf 
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 class velocity_controller_node(object):
     """woof woof"""
@@ -19,28 +21,89 @@ class velocity_controller_node(object):
 
     def tf_callback(self, msg):
         # print(msg)
-        pass     
+
+        pass
+
+    def transform_velocity(self, v, w):
+        def screw(a):
+            return np.array([[0, -a[2], a[1]],
+                    [a[2], 0, -a[0]],
+                    [-a[1], a[0], 0]])
+
+        try:
+            (p, R) = velocity_node.listener.lookupTransform('root', 'j2s7s300_link_finger_tip_1', rospy.Time(0))
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return None, None
+
+        p = np.array(p)
+        R = np.array(Rotation.from_quat(R).as_dcm()) # as_matrix for python3
+
+        v_goal = np.matmul(R, np.array([v]).T)
+        w_goal = np.array([[0.0, 0.0, 0.0]]).T
+
+        try:
+            (p, R) = velocity_node.listener.lookupTransform('j2s7s300_end_effector', 'j2s7s300_link_finger_tip_1', rospy.Time(0))
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return None, None
+
+        p = np.array(p)
+        R = np.array(Rotation.from_quat(R).as_dcm()) # as_matrix for python3
+
+        v_goal += np.matmul(np.matmul(screw(p),R), np.array([w]).T)
+
+        print(R.shape)
+        print(np.array([w]).T.shape)
+
+        w_goal += np.matmul(R, np.array([w]).T)
+
+        return v_goal, w_goal
 
 if __name__ == '__main__': 
     try:
         rospy.init_node('velocity_controller', anonymous=True)
         velocity_node = velocity_controller_node()
-        rate = rospy.Rate(100) # 100hz is required by the Kinva Arm
+        rate = rospy.Rate(20) # 100hz is required by the Kinva Arm
+
+        counter = 0
 
         while not rospy.is_shutdown():
+
             msg = kinova_msgs.msg.PoseVelocity()
-            msg.twist_angular_z = -.2
+
+            if counter < 100:
+
+                v_goal, w_goal = velocity_node.transform_velocity([0,0,0], [0,0.5,0])
+                # v_goal = np.array([[0,0,0]]).T
+                # w_goal = np.array([[0,0.5,0]]).T
+
+            elif counter < 200:
+
+                v_goal, w_goal = velocity_node.transform_velocity([0, 0,0], [0,-0.5,0])
+                # v_goal = np.array([[0,0,0]]).T
+                # w_goal = np.array([[0,-0.5,0]]).T
+
+            else: 
+                counter = 0
+                continue 
+
+            if v_goal is None:
+                continue 
+
+            msg.twist_linear_x = v_goal[0,0]
+            msg.twist_linear_y = v_goal[1,0]        
+            msg.twist_linear_z = v_goal[2,0]
+
+            msg.twist_angular_x = w_goal[0,0]
+            msg.twist_angular_y = w_goal[1,0]                
+            msg.twist_angular_z = w_goal[2,0]
+
+            print(msg)
+
             velocity_node.pub_velocity.publish(msg)
 
-            (trans,rot) = velocity_node.listener.lookupTransform('j2s7s300_link_finger_3', 'j2s7s300_link_end_effector', rospy.Time(0))
-
-            try:
-                (trans,rot) = velocity_node.listener.lookupTransform('j2s7s300_link_finger_3', 'j2s7s300_link_end_effector', rospy.Time(0))
-                print(trans)
-
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                continue
-
+            counter += 1
 
             rate.sleep() # ROS sleep once
 
